29595f202dd961d6621615ce4bd7320bbd2c52fc
Diana Yamileika Tellez Ordoñez <160613638+Yamileika@users.noreply.github.com>
2024-02-22 03:05:36 -0600

    Update programa02.py

--- programa02.py
+++ programa02.py
@@ -1,1 +1,12 @@
+"""
+Nombre: Diana Yamileika TO.
+Fecha: 18/01/2023
+Descripcion: Ejercicio de clase.
+"""
+class A: #Define una clase de nombre A
+    matricula=None #Declara la variable matricula
+    nombre=None #Declara la variable nombre
+    def __init__(self): #Define el metodo init
+        print("Constructor de la clase A") #Imprime el mensaje
 
+objetoA = A() #Crea un objeto de la clase A e invoca al metodo init
