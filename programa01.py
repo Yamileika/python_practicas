f80dd5633cbff67d57b6b6335c317007ec3618bd
Diana Yamileika Tellez Ordoñez <160613638+Yamileika@users.noreply.github.com>
2024-02-22 02:43:34 -0600

    Create programa01.py

---
+++ programa01.py
@@ -0,0 +1,11 @@
+"""
+Nombre: Diana Yamileika Tellez Ordoñez
+Fecha: 18/01/2023
+Descripcion: programa01.py
+"""
+class A: #Define una clase de nombre A
+    matricula=None #Declara la variable matricula
+    nombre=None #Declara la variable nombre
+
+objetoA = A ( ) #Crea un objeto de la clase A
+print(objetoA.nombre) #Imprime el valor de la variable nombre
